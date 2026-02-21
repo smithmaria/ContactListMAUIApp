8b26c6557385412621836cc215258c5826c193b9
Maria Smith <maria.t.smith023@gmail.com>
2026-02-20 19:35:54 -0500

    add contact list with data

---
+++ ContactListApp/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactListApp
+{
+    public class Contact
+    {
+        public string ProfileImage { get; set; }
+        public string Name { get; set; }
+    }
+}
--- ContactListApp/MainPage.xaml.cs
+++ ContactListApp/MainPage.xaml.cs
@@ -2,23 +2,35 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+
+            var contacts = new List<Contact>
+            {
+                new Contact { ProfileImage = "pic2.png", Name = "Audrey" },
+                new Contact { ProfileImage = "pic1.png", Name = "Connor" },
+                new Contact { ProfileImage = "pic3.png", Name = "Alex" },
+                new Contact { ProfileImage = "pic4.png", Name = "Tatiana" },
+                new Contact { ProfileImage = "pic2.png", Name = "Renee" },
+                new Contact { ProfileImage = "pic1.png", Name = "Parker" },
+                new Contact { ProfileImage = "pic3.png", Name = "George" },
+                new Contact { ProfileImage = "pic2.png", Name = "Olivia" },
+                new Contact { ProfileImage = "pic1.png", Name = "Tommy" },
+                new Contact { ProfileImage = "pic4.png", Name = "Taylor" },
+                new Contact { ProfileImage = "pic2.png", Name = "Caroline" },
+                new Contact { ProfileImage = "pic3.png", Name = "Trevor" },
+                new Contact { ProfileImage = "pic4.png", Name = "Elaine" },
+                new Contact { ProfileImage = "pic2.png", Name = "Lauren" },
+                new Contact { ProfileImage = "pic4.png", Name = "Ashley" }
+            };
+
+            contactsCltnView.ItemsSource = contacts;
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void contactsCltnView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Console.WriteLine("New contact selected");
         }
     }
 }
