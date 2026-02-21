cf6c0298f1960545baf1ffa76cfc9b2b83fcdcea
Maria Smith <maria.t.smith023@gmail.com>
2026-02-20 21:34:03 -0500

    add contact list groups

--- ContactListApp/MainPage.xaml.cs
+++ ContactListApp/MainPage.xaml.cs
@@ -8,24 +8,29 @@
 
             var contacts = new List<Contact>
             {
-new Contact { ProfileImage = "pic2.png", Name = "Audrey Bennett", Email = "audrey@email.com", Phone = "555-201-3456", Description = "Childhood friend" },
-new Contact { ProfileImage = "pic1.png", Name = "Connor Walsh", Email = "connor@email.com", Phone = "555-302-4567", Description = "Work colleague" },
-new Contact { ProfileImage = "pic3.png", Name = "Alex Rivera", Email = "alex@email.com", Phone = "555-403-5678", Description = "Gym buddy" },
-new Contact { ProfileImage = "pic4.png", Name = "Tatiana Volkov", Email = "tatiana@email.com", Phone = "555-504-6789", Description = "Neighbor" },
-new Contact { ProfileImage = "pic2.png", Name = "Renee Fontaine", Email = "renee@email.com", Phone = "555-605-7890", Description = "College roommate" },
-new Contact { ProfileImage = "pic1.png", Name = "Parker Holloway", Email = "parker@email.com", Phone = "555-706-8901", Description = "Family friend" },
-new Contact { ProfileImage = "pic3.png", Name = "George Malone", Email = "george@email.com", Phone = "555-807-9012", Description = "Book club member" },
-new Contact { ProfileImage = "pic2.png", Name = "Olivia Chen", Email = "olivia@email.com", Phone = "555-908-0123", Description = "Coworker" },
-new Contact { ProfileImage = "pic1.png", Name = "Tommy Briggs", Email = "tommy@email.com", Phone = "555-109-1234", Description = "High school friend" },
-new Contact { ProfileImage = "pic4.png", Name = "Taylor Sinclair", Email = "taylor@email.com", Phone = "555-210-2345", Description = "Yoga classmate" },
-new Contact { ProfileImage = "pic2.png", Name = "Caroline Moore", Email = "caroline@email.com", Phone = "555-311-3456", Description = "Sister" },
-new Contact { ProfileImage = "pic3.png", Name = "Trevor Nash", Email = "trevor@email.com", Phone = "555-412-4567", Description = "Tennis partner" },
-new Contact { ProfileImage = "pic4.png", Name = "Elaine Marsh", Email = "elaine@email.com", Phone = "555-513-5678", Description = "Mentor" },
-new Contact { ProfileImage = "pic2.png", Name = "Lauren Calloway", Email = "lauren@email.com", Phone = "555-614-6789", Description = "Cousin" },
-new Contact { ProfileImage = "pic4.png", Name = "Ashley Tran", Email = "ashley@email.com", Phone = "555-715-7890", Description = "Best friend" }
+                new Contact { ProfileImage = "pic2.png", Name = "Audrey Bennett", Email = "audrey@email.com", Phone = "555-201-3456", Description = "Childhood friend" },
+                new Contact { ProfileImage = "pic1.png", Name = "Connor Walsh", Email = "connor@email.com", Phone = "555-302-4567", Description = "Work colleague" },
+                new Contact { ProfileImage = "pic3.png", Name = "Alex Rivera", Email = "alex@email.com", Phone = "555-403-5678", Description = "Gym buddy" },
+                new Contact { ProfileImage = "pic4.png", Name = "Tatiana Volkov", Email = "tatiana@email.com", Phone = "555-504-6789", Description = "Neighbor" },
+                new Contact { ProfileImage = "pic2.png", Name = "Renee Fontaine", Email = "renee@email.com", Phone = "555-605-7890", Description = "College roommate" },
+                new Contact { ProfileImage = "pic1.png", Name = "Parker Holloway", Email = "parker@email.com", Phone = "555-706-8901", Description = "Family friend" },
+                new Contact { ProfileImage = "pic3.png", Name = "George Malone", Email = "george@email.com", Phone = "555-807-9012", Description = "Book club member" },
+                new Contact { ProfileImage = "pic2.png", Name = "Olivia Chen", Email = "olivia@email.com", Phone = "555-908-0123", Description = "Coworker" },
+                new Contact { ProfileImage = "pic1.png", Name = "Tommy Briggs", Email = "tommy@email.com", Phone = "555-109-1234", Description = "High school friend" },
+                new Contact { ProfileImage = "pic4.png", Name = "Taylor Sinclair", Email = "taylor@email.com", Phone = "555-210-2345", Description = "Yoga classmate" },
+                new Contact { ProfileImage = "pic2.png", Name = "Caroline Moore", Email = "caroline@email.com", Phone = "555-311-3456", Description = "Sister" },
+                new Contact { ProfileImage = "pic3.png", Name = "Trevor Nash", Email = "trevor@email.com", Phone = "555-412-4567", Description = "Tennis partner" },
+                new Contact { ProfileImage = "pic4.png", Name = "Elaine Marsh", Email = "elaine@email.com", Phone = "555-513-5678", Description = "Mentor" },
+                new Contact { ProfileImage = "pic2.png", Name = "Lauren Calloway", Email = "lauren@email.com", Phone = "555-614-6789", Description = "Cousin" },
+                new Contact { ProfileImage = "pic4.png", Name = "Ashley Tran", Email = "ashley@email.com", Phone = "555-715-7890", Description = "Best friend" }
             };
 
-            contactsCltnView.ItemsSource = contacts;
+            var grouped = contacts
+                .GroupBy(c => c.Name[0].ToString())
+                .OrderBy(g => g.Key)
+                .Select(g => new ContactGroup(g.Key, g.ToList()))
+                .ToList();
+            contactsCltnView.ItemsSource = grouped;
         }
 
         private async void contactsCltnView_SelectionChanged(object sender, SelectionChangedEventArgs e)
